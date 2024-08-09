60b11935356f3c1ed86ffaf2a9012f54f7dd3d8a
Yu <victoryyue@gmail.com>
2024-08-09 14:09:50 +0800

    change base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/20240809-project/',
+  base: '/20240809-shopping/',
   plugins: [
     vue(),
   ],
