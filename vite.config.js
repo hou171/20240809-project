bed5c3b1c4aea9671b628b50430e2adda800cc74
hou171 <31945646+hou171@users.noreply.github.com>
2024-09-10 23:51:49 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/20240809-shopping/',
+  base: '/shopping/',
   plugins: [
     vue(),
   ],
